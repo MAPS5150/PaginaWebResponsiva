8c2300ed6690920c9fc3a3882a33f9d435b3d0e9
Miguel Perez <dauphin.swim@gmail.com>
2022-10-20 23:13:09 -0500

    Commit cambios

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/*API URL*/
+const API_URL="https://jsonplaceholder.typicode.com";
+
 const workItems = document.querySelectorAll('.work .item');
 const workItemsCount = workItems.length;
 let currentIndex = 0;
@@ -7,6 +10,12 @@ const nextButton = document.querySelector("#next-button");
 const openCloseButton = document.querySelector("#buttonOpenCloseMenu");
 const menuMobileItems = document.querySelector("#menu-mobile-items");
 
+/*Response API*/
+
+const HTMLResponse = document.querySelector('#app');
+const ul = document.createElement('ul');
+
+/*--------------------------------------------------------------------------*/
 openCloseButton.addEventListener("click", e => {
     menuMobileItems.classList.toggle("menu-mobile-closed");
 });
@@ -78,3 +87,16 @@ function loadGalleryItem(index) {
     items.forEach(item => {item.classList.add('item-hide')});
     items[index].classList.toogle('item-hide');
 }
+
+/*-----------------------------Fetch para la API---------------------------*/
+fetch(`${API_URL}/users`)
+    .then((response) => response.json())
+    .then((users) => {
+        users.forEach(user => {
+            let elem = document.createElement('li');
+            elem.appendChild(document.createTextNode(`${user.name} , ${user.email}`));
+            ul.appendChild(elem);
+        });
+
+        HTMLResponse.appendChild(ul);
+    });
