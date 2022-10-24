3a856517e25272a47771ac622583ab23cbf5f7cc
Miguel Perez <dauphin.swim@gmail.com>
2022-10-23 19:13:55 -0500

    update23Oct22

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 /*API URL*/
-const API_URL="https://jsonplaceholder.typicode.com";
+// const API_URL="https://reqres.in/api/users/";
+//const API_URL="https://jsonplaceholder.typicode.com";
+//const API_URL = "https://pokeapi.co/api/v2/pokemon/1/"
 
+
+/*Botones para crear la interaccion en el sitio web*/
 const workItems = document.querySelectorAll('.work .item');
 const workItemsCount = workItems.length;
 let currentIndex = 0;
@@ -12,13 +16,14 @@ const menuMobileItems = document.querySelector("#menu-mobile-items");
 
 /*Response API*/
 
-const HTMLResponse = document.querySelector('#app');
-const ul = document.createElement('ul');
+//const HTMLResponse = document.querySelector('#app');
+//const ul = document.createElement('ul');
 
-/*--------------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------*/
+
 openCloseButton.addEventListener("click", e => {
-    menuMobileItems.classList.toggle("menu-mobile-closed");
-});
+        menuMobileItems.classList.toggle("menu-mobile-closed");
+    });
 
 workItems.forEach(item => {
     item.addEventListener('click', e => {
@@ -27,14 +32,9 @@ workItems.forEach(item => {
 
         const contentArr = document.querySelectorAll("#details-container .item");
 
-        document.querySelectorAll("#details-container .item").forEach(item => {
-            item.classList.add('item-hide');
-        });
-
+        document.querySelectorAll("#details-container .item").forEach(item => {item.classList.add('item-hide');})
         document.querySelectorAll("#details-container .item")[currentIndex].classList.toggle('item-hide');
-
-	    document.querySelector('#screen').style['animation-name'] = 'fade-in';
-
+        document.querySelector('#screen').style['animation-name'] = 'fade-in';   //cambio de pantalla con una ventana
         document.querySelector('body').style['overflow'] = 'hidden';
 
         setTimeout(() => {
@@ -50,31 +50,30 @@ workItems.forEach(item => {
 closeButton.addEventListener('click', e => {
     e.preventDefault();
 
-        document.querySelector('#screen').style['animation-name'] = 'fade-in';
+    document.querySelector('#screen').style['animation-name'] = 'fade-in';
+    document.querySelector('body').style['overflow'] = 'auto';
 
-        document.querySelector('body').style['overflow'] = 'auto';
+    setTimeout(() => {
+        document.querySelector("#details-container").style.display = 'none';   
+    }, 1000);
 
-        setTimeout(() => {
-            document.querySelector("#details-container").style.display = 'none';   
-        }, 1000);
-
-        setTimeout (() => {
-            document.querySelector("#screen").style = '';
-        }, 2000);
+    setTimeout (() => {
+        document.querySelector("#screen").style = '';
+    }, 2000);
 });
 
 prevButton.addEventListener('click', e => {
     e.preventDefault();
-        if (currentIndex - 1 < 0) {
-            return;
-        }
-        currentIndex--;
-        loadGalleryItem(currentIndex);
+    if (currentIndex - 1 < 0) {
+        return;
+    }
+    currentIndex--;
+    loadGalleryItem(currentIndex);
     });
 
 nextButton.addEventListener('click', e => {
     e.preventDefault();
-    if(currentIndex + 1 == workItemsCount) {
+    if(currentIndex + 1 === workItemsCount) {
         return;
     }
     currentIndex++;
@@ -85,18 +84,100 @@ function loadGalleryItem(index) {
     const items = document.querySelectorAll('#details-container .item');
 
     items.forEach(item => {item.classList.add('item-hide')});
-    items[index].classList.toogle('item-hide');
+    items[index].classList.toggle('item-hide');
 }
 
 /*-----------------------------Fetch para la API---------------------------*/
-fetch(`${API_URL}/users`)
-    .then((response) => response.json())
-    .then((users) => {
-        users.forEach(user => {
-            let elem = document.createElement('li');
-            elem.appendChild(document.createTextNode(`${user.name} , ${user.email}`));
-            ul.appendChild(elem);
-        });
 
-        HTMLResponse.appendChild(ul);
+const listUsers = async () => {
+    const response = await fetch("https://reqres.in/api/users/");
+    const users = await response.json();   //accedemos al json
+
+    let tableBody = ``;
+    users.data.forEach((user, index) => {
+        //console.log(user);
+        tableBody += `<tr>
+        <td>${user.id}</td>
+        <td>${user.email}</td>
+        <td>${user.first_name}</td>
+        <td>${user.last_name}</td>
+        <td><img src="${user.avatar}"></td>
+        <tr>`;
     });
+
+    document.getElementById("tableBody_Users").innerHTML = tableBody;
+};
+
+window.addEventListener("load", function() {
+    listUsers();
+});
+
+
+
+
+
+//const tabla = document.querySelector('#app');
+
+
+/*fetch(API_URL)
+    .then(res => res.json())
+    .then(datos => {
+                let element =document.getElementById('app')
+                element.innerHTML = `
+                <p>${datos.data.first_name}</p>
+                <p>${datos.data.email}</p>
+                <img src="${datos.data.avatar}">`;
+    });*/
+//let element =document.getElementById('app');
+
+//element.onclick = () => {
+  //  getData();
+//}
+
+//const getData = async () => {
+//    await fetch(API_URL).then((response) => {
+//        return response.json();}).then((datos) => {
+//            console.log(datos);
+        
+
+//        for(let i = 0; i < datos.length; i++) {
+            //creamos una etiqueta html de imagen
+//            const fotos = docment.createElement('img');
+            //creamos un objeto para guardar las imagenes
+//            fotos.src = datos.data[i].images["original"].API_URL;
+//            fotos.className = "nb-3";
+//            document.getElementById("app").appendChild(fotos);
+
+//        }
+//    })
+    
+//}
+
+//getData();
+//async function obtenerDatos() {
+//    const response = await fetch(API_URL);
+//    const json = await response.json();
+    
+
+//    for (let elemento = 0; elemento <= json.data.length; elemento++) {
+//
+//        element.innerHTML = `
+//        <p>${elemento.first_name}</p>
+//        <p>${elemento.email}</p>
+//        <img src="${elemento.avatar}">`;
+
+   /*Mi Objeto principal*/
+    //console.log(json);
+    //console.log(json.data[0].first_name);
+    //console.log(json.data[1].first_name);
+//    json.data.forEach(elemento => {
+//        let element =document.getElementById('app')
+
+//        element.innerHTML = `
+//                <p>${elemento.first_name}</p>
+//                <p>${elemento.email}</p>
+//                <img src="${elemento.avatar}">`;
+//    });
+
+
+//obtenerDatos();
